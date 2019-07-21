f1c7dfab202c233d08e03f7aa7b64a5d66f86313
Dmitriy Pleshevskiy <dmitriy@ideascup.me>
2019-07-21 21:40:38 +0300

    chore: fix readme

--- setup.py
+++ setup.py
@@ -8,11 +8,12 @@ with open('README.md', 'r') as f:
 if __name__ == '__main__':
     setup(
         name='rusn',
-        version='1.0.0',
+        version='1.0.1',
         author='Dmitriy Pleshevskiy',
         author_email='dmitriy@ideascup.me',
         description='Small library for getting russian text with singular and plural forms',
         long_description=readme,
+        long_description_content_type='text/markdown',
         package_data={'': ['LICENSE', 'README.md']},
         include_package_data=True,
         license='MIT',
